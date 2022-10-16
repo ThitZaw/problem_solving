1ff6e618eca32d028cf7c79c8cb3f33df162e149
ThitZaw <ThitZaw@users.noreply.github.com>
2022-10-16 12:48:25 +0630

    modify insert interval

--- grind_75/Array/insert_interval.py
+++ grind_75/Array/insert_interval.py
@@ -6,26 +6,20 @@ class Solution:
     def insert(self, intervals, newInterval):
         res = []
         for i in range(len(intervals)):
-            # 1
-            # [4,8] > [1,2] # 8 is greater than 2 
-            # 5
-            # [3,10] < [12,16] no overlapping
+            # __________         _____________________________________
+            # |_(s ,e)_|    |_(s ,e)_|_(s ,e)_|_(s ,e)_|_(s ,e)_|
+            #   new[1]  <   interval[i][0]
             if newInterval[1] < intervals[i][0]:
-                res.append(newInterval) # append (3,10)
-                return res + intervals[i:] # append rest of the greater interval [12,16]
-            # 1
-            # [1,2],[3,5],[6,7],[8,10],[12,16]
-            #     [4,8]   greater than 2 but start = 4 is less than end = 5 
+                res.append(newInterval)
+                return res + intervals[i:]
+            # _____________________________________    __________
+            # |_(s ,e)_|_(s ,e)_|_(s ,e)_|_(s ,e)_|    |_(s ,e)_|
+            #                        interval[i][1]  <  new[0]
             elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i]) # append [1,2]
-            else: # overlapped interval
+                res.append(intervals[i])
+            # overlapped between interval
+            else:
                 newInterval = [min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])] 
-                # 2
-                # min(3,4),max(5,8) => [3,8],
-                # 3
-                # min(3,6),max(7,8) => [3,8],
-                # 4
-                # min(3,8),max(8,10) => [3,10]
         res.append(newInterval)
         return res
     
